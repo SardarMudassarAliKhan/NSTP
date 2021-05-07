b04af8fc88181fdbfaa26c4c22b136e18927da5c
Mudassar Ali Khan <mkhan@vaporvm.com>
2021-05-08 02:06:21 +0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NSTP.Startup))]
+namespace NSTP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
